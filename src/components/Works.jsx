62d6fa4316ae0130561445fc390cf1ecea85fdc0
Akram Helali <akramhelali007@outlook.com>
2023-03-26 23:30:26 +0100

    adding the paragraph

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -12,7 +12,10 @@ const Works = () => {
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>MY WORK</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
-      </motion.div>    
+      </motion.div>
+      <motion.p variants={fadeIn("","", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+      </motion.p> 
     </>
   )
 }
